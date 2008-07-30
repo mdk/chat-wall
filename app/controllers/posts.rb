a9388e64717bc7f86fcd04b8379537cfcad1dc25
Michael Dominic K <mdk@mdk.am>
2008-07-30 18:28:06 +0200

    Removing some old broken methods polluting the posts controller.

--- app/controllers/posts.rb
+++ app/controllers/posts.rb
@@ -4,22 +4,7 @@ class Posts < Application
     @posts = Post.find(:all, :order => "created_at DESC")
     render
   end
-  
-  def json_index
-    @posts = Post.find(:all, :order => "created_at DESC")
-    render :js => @posts.to_jsonn
-  end
-  
-  def sample
-    p = Post.find(:all) [0]
-    partial("/shared/post", :with => p)
-  end
-  
-  def create
-    Post.create!(:body => params[:body])
-    redirect url(:action => "index")
-  end
-  
+
   def dynamic_create
     p = Post.create(:body => params[:body])
     partial("/shared/post", :with => p)
