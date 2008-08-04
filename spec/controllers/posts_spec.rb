54aacbcb0d6b9cb872a97b2ffa308f15bff02f11
Michael Dominic K <mdk@mdk.am>
2008-08-04 09:00:53 +0200

    Adding a few more tests.

--- spec/controllers/posts_spec.rb
+++ spec/controllers/posts_spec.rb
@@ -6,3 +6,23 @@ describe Posts, "#index" do
     dispatch_to(Posts, :index).should respond_successfully
   end
 end
+
+describe Posts, "#dynamic_create" do
+
+  before(:each) do
+    @params = { :body => "This is a test" }
+  end
+  
+  it "should respond correctly" do
+    dispatch_to(Posts, :dynamic_create, @params).should respond_successfully
+  end
+
+  it "should change post count" do
+    lambda { dispatch_to(Posts, :dynamic_create, @params) }.should change(Post, :count)
+  end
+
+  it "should render proper HTML content" do
+    pending "Change the dynamic_create to use proper view rendering"
+  end
+
+end
